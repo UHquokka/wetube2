bb4686b1ede01f361c468adec9c20fb2fffa7290
UHquokka <100435210+UHquokka@users.noreply.github.com>
2022-11-06 01:27:13 +0900

    middleware 과 next 인자 사용법

--- src/server.js
+++ src/server.js
@@ -4,11 +4,16 @@ const app = exress();
 
 const PORT = 5555;
 
+const gossipMiddleWare = (req, res, next) => {
+  console.log("I'm in middle");
+  next();
+};
+
 const handleHome = (req, res) => {
   return res.send("I still love you");
 };
 
-app.get("/", handleHome);
+app.get("/", gossipMiddleWare, handleHome);
 
 app.listen(5555, () =>
   console.log(`✅ Server listening on port http://localhost:${PORT} 🍋`)
