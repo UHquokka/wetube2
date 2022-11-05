6a47c2b9ea2c378bf0c326775081bf06c9acddc9
UHquokka <100435210+UHquokka@users.noreply.github.com>
2022-11-06 03:06:11 +0900

    protected page 생성

--- src/server.js
+++ src/server.js
@@ -4,8 +4,17 @@ const app = exress();
 
 const PORT = 5555;
 
-const gossipMiddleWare = (req, res, next) => {
-  console.log("I'm in middle");
+const logger = (req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+};
+
+const privateMiddelware = (req, res, next) => {
+  const url = req.url;
+  if (url === "/protected") {
+    return res.send("<h1>Not Allowed</h1>");
+  }
+  console.log("Allowed, you may continue");
   next();
 };
 
@@ -13,7 +22,14 @@ const handleHome = (req, res) => {
   return res.send("I still love you");
 };
 
-app.get("/", gossipMiddleWare, handleHome);
+const handleProtected = (req, res) => {
+  return res.send("welcome to the private rounge");
+};
+app.use(logger);
+app.use(privateMiddelware);
+
+app.get("/", handleHome);
+app.get("/procted", handleProtected);
 
 app.listen(5555, () =>
   console.log(`✅ Server listening on port http://localhost:${PORT} 🍋`)
