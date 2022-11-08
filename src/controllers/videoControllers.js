b3aa2beda93c04fdb0d4a1d1289ce3f6e00ccc49
UHquokka <100435210+UHquokka@users.noreply.github.com>
2022-11-08 14:49:56 +0900

    #5.6 까지 마무리 - 오류, 오타 해결

--- src/controllers/videoControllers.js
+++ src/controllers/videoControllers.js
@@ -5,8 +5,9 @@ export const search = (req, res) => res.send("Search Videos");
 //videoRouter
 export const see = (req, res) => {
   console.log(req.params);
-  return res.render("watch");
+  return res.render("watch", { pageTitle: "Watch Video" });
 };
-export const edit = (req, res) => res.render("edit");
+export const edit = (req, res) =>
+  res.render("edit", { pageTitle: "Edit Video" });
 export const upload = (req, res) => res.send("Upload Video");
 export const deleteVideo = (req, res) => res.send("Delete Video");
