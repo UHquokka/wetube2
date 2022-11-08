381a5698fe4ac7e1b3da4c12f6632d7e7b8747da
UHquokka <100435210+UHquokka@users.noreply.github.com>
2022-11-08 12:11:37 +0900

    pug 일부 페이지 render

--- src/controllers/videoControllers.js
+++ src/controllers/videoControllers.js
@@ -1,12 +1,12 @@
 //globalRouter
-export const treding = (req, res) => res.send("Home Page Videos");
+export const treding = (req, res) => res.render("home");
 export const search = (req, res) => res.send("Search Videos");
 
 //videoRouter
 export const see = (req, res) => {
   console.log(req.params);
-  return res.send("Videos");
+  return res.render("watch");
 };
-export const edit = (req, res) => res.send("Edit Videos");
+export const edit = (req, res) => res.render("edit");
 export const upload = (req, res) => res.send("Upload Video");
 export const deleteVideo = (req, res) => res.send("Delete Video");
