181ab064cd29a99100a0f599bdad8318c7629eb5
UHquokka <100435210+UHquokka@users.noreply.github.com>
2022-11-15 16:08:00 +0900

    가입시 오류메세지 출력
    username, email 중복 오류,
    password 매치 오류

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -2,10 +2,24 @@ import User from "../models/User";
 
 //global Router
 export const getJoin = (req, res) => {
-  return res.render("join", { pageTitle: "Create Account" });
+  return res.render("join", { pageTitle: "Join" });
 };
+
 export const postJoin = async (req, res) => {
-  const { name, username, email, password, location } = req.body;
+  const { name, username, email, password, password2, location } = req.body;
+  if (password !== password2) {
+    res.render("join", {
+      pageTitle: "Join",
+      errorMessage: "Password confirmation does not match",
+    });
+  }
+  const exist = await User.exists({ $or: [{ username }, { email }] });
+  if (exist) {
+    res.render("join", {
+      pageTitle: "Join",
+      errorMessage: "This username/email is already taken",
+    });
+  }
   await User.create({
     name,
     username,
