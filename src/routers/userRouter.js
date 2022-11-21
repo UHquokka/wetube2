a28790ac48eab2132208524b56fab30630f730f8
UHquokka <100435210+UHquokka@users.noreply.github.com>
2022-11-21 23:15:20 +0900

    페이지 방문 제한을 위한 미들웨어 생성

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,18 +1,20 @@
 // URL이 /users 로 시작
 import express from "express";
 import {
-  editUser,
+  getEdit,
+  postEdit,
   logout,
   see,
   startGithubLogin,
   finishGithubLogin,
 } from "../controllers/userControllers";
+import { protectedMiddleware, publicOnlyMiddleware } from "../middlewares";
 const userRouter = express.Router();
 
-userRouter.get("/logout", logout);
-userRouter.get("/edit", editUser);
-userRouter.get("/github/start", startGithubLogin);
-userRouter.get("/github/finish", finishGithubLogin);
+userRouter.get("/logout", protectedMiddleware, logout);
+userRouter.route("/edit").all(protectedMiddleware).get(getEdit).post(postEdit);
+userRouter.get("/github/start", publicOnlyMiddleware, startGithubLogin);
+userRouter.get("/github/finish", publicOnlyMiddleware, finishGithubLogin);
 userRouter.get(":id", see);
 
 export default userRouter;
