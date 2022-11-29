a06b26330ba0e9ed1e3512db6e0286b042887b29
UHquokka <100435210+UHquokka@users.noreply.github.com>
2022-11-29 17:28:07 +0900

    player의 play, muted Btn 생성

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -1,1 +1,27 @@
-console.log("Video Player");
+const video = document.querySelector("video");
+const playBtn = document.getElementById("play");
+const muteBtn = document.getElementById("mute");
+const time = document.getElementById("time");
+const volumnRange = document.getElementById("volumn");
+
+const handlePlayClick = (e) => {
+  if (video.paused) {
+    video.play();
+  } else {
+    video.pause();
+  }
+  playBtn.innerText = video.pause ? "Play" : "Puase";
+};
+
+const handleMute = (e) => {
+  if (video.muted) {
+    video.muted = false;
+  } else {
+    video.muted = true;
+  }
+  muteBtn.innerText = video.muted ? "UnMute" : "Mute";
+  volumnRange.value = video.muted ? 0 : 0.5;
+};
+
+play.addEventListener("click", handlePlayClick);
+muteBtn.addEventListener("click", handleMute);
