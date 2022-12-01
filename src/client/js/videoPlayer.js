378edb1b28697ada969c2196c6022a56b63a6f08
UHquokka <100435210+UHquokka@users.noreply.github.com>
2022-12-01 18:50:39 +0900

    data attribute로 HTML에 data 저장하기

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -117,6 +117,7 @@ volumeRange.addEventListener("input", handleVolumeChange);
 video.addEventListener("loadeddata", handleLoadedMetadata);
 video.addEventListener("timeupdate", handleTimeUpdate);
 video.addEventListener("click", handlePlayClick);
+video.addEventListener("ended", handleEnded);
 videoContainer.addEventListener("mousemove", handleMouseMove);
 videoContainer.addEventListener("mouseleave", handleMouseLeave);
 timeline.addEventListener("input", handleTimelineChange);
