752ee7202fccf3e76e15d15ef2a379fc10be019b
UHquokka <100435210+UHquokka@users.noreply.github.com>
2022-12-04 23:38:24 +0900

    acl 수정

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -152,7 +152,6 @@ export const postEdit = async (req, res) => {
     body: { name, email, username, location },
     file,
   } = req;
-  console.log(file);
   const updateUser = await User.findByIdAndUpdate(
     _id,
     {
