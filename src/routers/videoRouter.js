a94704e3ed114437a2b53bf4004711097499d55d
UHquokka <100435210+UHquokka@users.noreply.github.com>
2022-12-06 11:42:26 +0900

    multer 오류

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -25,6 +25,6 @@ videoRouter
   .route("/upload")
   .all(protectedMiddleware)
   .get(getUpload)
-  .post(videoUpload.single("name: video"), postUpload);
+  .post(videoUpload.single("video"), postUpload);
 
 export default videoRouter;
